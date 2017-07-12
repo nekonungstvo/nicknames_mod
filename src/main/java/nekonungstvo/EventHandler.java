72e8a66a287fb55d50024917580fd36a427ce6b1
xunto <xunto@gmail.com>
2017-07-12 16:53:06 +0300

    Get ClientPlayer from FMLClientHandler

--- src/main/java/nekonungstvo/EventHandler.java
+++ src/main/java/nekonungstvo/EventHandler.java
@@ -1,9 +1,9 @@
 package nekonungstvo;
 
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderLivingEvent;
 
@@ -12,7 +12,7 @@ public class EventHandler {
     @SideOnly(Side.CLIENT)
     public void nickname_draw(RenderLivingEvent.Specials.Pre event) {
         if (event.entity instanceof EntityPlayer) {
-            EntityPlayer current_player = Minecraft.getMinecraft().thePlayer;
+            EntityPlayer current_player = FMLClientHandler.instance().getClientPlayerEntity();
             EntityPlayer player = (EntityPlayer) event.entity;
             boolean hide_name = !player.canEntityBeSeen(current_player);
             if (event.isCancelable() && hide_name) {
